ad4dcf2d0730c73926a4ec0453908bea77075847
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-03 16:15:43 -0500

    Update ADCSF.py

--- scripts/adcsf-main/ADCSF.py
+++ scripts/adcsf-main/ADCSF.py
@@ -43,7 +43,7 @@ with models.DAG(
     script = DockerOperator(  
         command="Rscript /opt/airflow/dags/repo/scripts/adcsf-main/ADCSF.R",
         image="harbor-atx.us.int.sonichealthcare/airflow/r-base:latest",
-        image_pull_secrets=regcred-atx,
+        # image_pull_secrets=regcred-atx,
         image_pull_secrets=[k8s.V1LocalObjectReference('regcred-atx')],
         force_pull=True,
         working_dir="/opt/airflow/dags/repo/scripts",
