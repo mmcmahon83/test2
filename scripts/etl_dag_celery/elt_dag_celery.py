aaeff904ea266c8793fa427166999f8c79122415
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-05 11:48:57 -0500

    Update elt_dag_celery.py

--- scripts/etl_dag_celery/elt_dag_celery.py
+++ scripts/etl_dag_celery/elt_dag_celery.py
@@ -1,13 +1,23 @@
+from __future__ import annotations
+
+import os
+from dotenv import load_dotenv
+from datetime import datetime, timedelta
+import pendulum
+from airflow import models
+from airflow.contrib.operators import kubernetes_pod_operator
+from airflow.providers.smtp.operators.smtp import EmailOperator
+from airflow.operators.python import PythonOperator
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators import kubernetes_pod_operator
 
-default_args = {
-    'owner': 'Damavis',
-    'start_date': datetime(2020, 5, 5),
-    'retries': 1,
-    'retry_delay': timedelta(seconds=5)
-}
+#default_args = {
+#    'owner': 'Damavis',
+#    'start_date': datetime(2020, 5, 5),
+#    'retries': 1,
+#    'retry_delay': timedelta(seconds=5)
+#}
 
 with DAG('etl_dag',
          default_args=default_args,
