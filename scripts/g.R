875be5ee4366b15693228cf9233f9dafcae5ed7a
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-05 15:54:34 -0500

    Update g.R

--- scripts/g.R
+++ scripts/g.R
@@ -1,6 +1,6 @@
 asst<-"OK"
 library('RJDBC')
-drv<-JDBC("com.kbs.jdbc.KBSDriver","/usr/local/jdbc/kbsjdbc18.jar","'")
+drv<-JDBC("com.kbs.jdbc.KBSDriver","/opt/airflow/dags/repo/scripts/kbsjdbc18.jar","'")
 asst<-tryCatch({
    conn<-dbConnect(drv, "jdbc:kbsjdbc://200.10.10.10:8600", "DBA", "SHARK")} , 
    warning=function(w) {return("WARN")}, error=function(e) {return("ERR")}, finally={})
