d60ceb675edc0855ac507e7c3d2279b27871c218
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-05 13:17:26 -0500

    Update elt_dag_celery.py

--- scripts/etl_dag_celery/elt_dag_celery.py
+++ scripts/etl_dag_celery/elt_dag_celery.py
@@ -22,7 +22,7 @@ default_args = {
 
 
 
-    vol1 = k8s.V1VolumeMount(name='test-volume', mount_path='/opt/airflow/dags')
+vol1 = k8s.V1VolumeMount(name='test-volume', mount_path='/opt/airflow/dags')
     volume = k8s.V1Volume(
             name='test-volume',
             persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name='airflow-dags'),
