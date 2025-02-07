fbdd68091be0587fcafafec943561d02e081b907
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-07 20:25:54 +0000

    Update ADCSF.py

--- scripts/adcsf-main/ADCSF.py
+++ scripts/adcsf-main/ADCSF.py
@@ -24,7 +24,7 @@ from pathlib import Path
 # dotenv_path = '/opt/airflow/dags/repo/scriptse/env.txt'
 # load_dotenv(dotenv_path)
 #load_dotenv(dotenv_path=dotenv_path)
-load_dotenv('/opt/airflow/dags/repo/scripts/.env')
+load_dotenv(/opt/airflow/dags/repo/scripts/.env)
 from docker.types import Mount 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "ADCSF" #update this, this will be the DAG name in Airflow
