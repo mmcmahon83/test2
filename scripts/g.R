49ab1ebe2e20ebd1bf43e5c160e50c610d61ca5a
mmcmahon83 <116818793+mmcmahon83@users.noreply.github.com>
2025-02-11 09:33:57 -0500

    Delete scripts directory

--- scripts/g.R
+++
@@ -1,6 +0,0 @@
-asst<-"OK"
-library('RJDBC')
-drv<-JDBC("com.kbs.jdbc.KBSDriver","/opt/airflow/dags/repo/scripts/kbsjdbc18.jar","'")
-asst<-tryCatch({
-   conn<-dbConnect(drv, "jdbc:kbsjdbc://200.10.10.10:8600", "DBA", "SHARK")} , 
-   warning=function(w) {return("WARN")}, error=function(e) {return("ERR")}, finally={})
